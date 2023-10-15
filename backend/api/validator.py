e68709feae05608b6c3f727c3a6bfdd84702fdbe
Бутенко Николай <e2e4e2e4@mail.ru>
2023-10-15 20:43:14 +0200

    Ревью 3

--- backend/api/validator.py
+++ backend/api/validator.py
@@ -1,10 +1,12 @@
 from django.core.exceptions import ValidationError
 
+from constants import MIN_COOKING_TIME, MAX_COOKING_TIME
+
 
 def cooking_time_validator(value):
     """Валидатор времени приготовления."""
     try:
-        if (int(value) <= 0) or (int(value) >= 1000):
+        if value <= MIN_COOKING_TIME or value >= MAX_COOKING_TIME:
             raise ValidationError('Неверно указано время!')
     except ValueError:
         raise ValidationError('Неверный формат')
